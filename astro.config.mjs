e90a5af0ceef3baa89b350d9b0163f3258c46e17
Ruchita Gosavi <70577616+Ruchita1010@users.noreply.github.com>
2025-10-07 01:53:42 +0530

    feat: add shiki themes for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'min-light',
+        dark: 'min-dark',
+      },
+      defaultColor: 'dark',
+    },
+  },
 });
