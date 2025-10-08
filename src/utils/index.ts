658d5e25caf7de1d783ca0b9da227fa6a92dadd5
Ruchita Gosavi <70577616+Ruchita1010@users.noreply.github.com>
2025-10-08 19:53:42 +0530

    refactor: improve Post layout and include day in date

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,10 @@
 // split YYYY-MM-DD to avoid UTC shifts
 export function formatDate(dateStr: string) {
-  const [year, month] = dateStr.split('-').map(Number);
-  const date = new Date(year, month - 1);
+  const [year, month, day] = dateStr.split('-').map(Number);
+  const date = new Date(year, month - 1, day);
   return date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'short',
+    day: '2-digit',
   });
 }
